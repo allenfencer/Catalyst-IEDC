25d1eefdd7aa06dad0a09b992cf9f9e5e1a1e991
Rahul Sunil <rahulsunil2@gmail.com>
2022-07-15 17:05:16 +0530

    index.html Fixed

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
 }
 
 //SLIDESHOW IN TEAM BANNER
@@ -23,13 +23,15 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+	let i;
+	let slides = document.getElementsByClassName("mySlides");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {
+		slideIndex = 1;
+	}
+	slides[slideIndex - 1].style.display = "block";
+	setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
